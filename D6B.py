1946226bd3eb71e9e317e0dc7b46f074255b210a
Dan6erbond <moravrav@gmail.com>
2019-06-30 09:29:44 +0200

    banhammer version bump to 1.8.0, added $addsub and $delsub, enhanced swear filter

--- D6B.py
+++ D6B.py
@@ -1,15 +1,16 @@
 import configparser
+import json
 
 import discord
 from discord.ext import commands
 
+import fuzzle
 import login
 from banhammer import banhammer
 
-desc = "Dan6erbond support bot."
-bot = commands.Bot("$", description=desc)
-bh = banhammer.Banhammer(login.get_reddit(), bot=bot, change_presence=True, embed_color=discord.Colour(0).from_rgb(67, 181, 129))
-bh.add_subreddits("dan6erbond")
+bot = commands.Bot("$", description="Dan6erbond support bot.")
+bh = banhammer.Banhammer(login.get_reddit(), bot=bot, change_presence=True,
+                         embed_color=discord.Colour(0).from_rgb(67, 181, 129))
 
 
 @bot.event
@@ -29,6 +30,10 @@ async def on_member_join(member):
 @bot.event
 async def on_ready():
     print(str(bot.user) + ' is running.')
+    with open("files/subreddits.json") as f:
+        subs = json.loads(f.read())
+        for sub in subs:
+            bh.add_subreddits(sub["sub"])
     bh.run()
 
 
@@ -42,38 +47,101 @@ async def reactions(ctx):
     await ctx.send(embed=bh.get_reactions_embed())
 
 
-@bh.new(subreddit="dan6erbond")
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def addsub(ctx, subreddit):
+    with open("files/subreddits.json") as f:
+        subs = json.loads(f.read())
+        for sub in subs:
+            if sub["sub"].lower() == subreddit.lower():
+                await ctx.send("/r/{} already added!".format(subreddit))
+                return
+        for cat in ctx.guild.categories:
+            if cat.id == 594461395039551519:
+                bh.add_subreddits(sub["sub"])
+                await ctx.guild.create_text_channel(subreddit, category=cat)
+                subs.append({
+                    "sub": subreddit,
+                    "id": channel.id
+                })
+                with open("files/subreddits.json", "w+") as f:
+                    f.write(json.dumps(subs, indent=4))
+                await ctx.send("Successfully added /r/{}!".format(subreddit))
+                break
+
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def delsub(ctx):
+    if ctx.channel.category is None or ctx.channel.category.id != 594461395039551519:
+        return
+    with open("files/subreddits.json") as f:
+        subs = json.loads(f.read())
+        found = False
+        for sub in subs:
+            if sub["sub"].lower() == ctx.channel.name.lower():
+                subs.remove(sub)
+                found = True
+                break
+        if found:
+            with open("files/subreddits.json", "w+") as f:
+                f.write(json.dumps(subs, indent=4))
+            await ctx.send("Successfully removed /r/{}!".format(ctx.channel.name))
+        else:
+            await ctx.send("/r/{} was never added!".format(ctx.channel.name))
+
+
+@bh.new()
+@bh.comments()
+@bh.mail()
 @bh.reports()
-async def handle_dan6erbond_new(post):
-    channel_id = 593131220041465868 if str(post.subreddit).lower() == "dan6erbond" else 593712450487058433 if str(
-        post.subreddit).lower() == "substarters" else 593816541464625205
-    msg = await bot.get_channel(channel_id).send(embed=post.get_embed())
-    for react in post.get_reactions():
-        try:
-            await msg.add_reaction(react.emoji)
-        except:
-            continue
+@bh.reports("Anti-Evil Operations")
+async def handle_item(item):
+    with open("files/subreddits.json") as f:
+        subs = json.loads(f.read())
+        for sub in subs:
+            if sub["sub"].lower() == str(item.subreddit).lower():
+                msg = await bot.get_channel(sub["id"]).send(embed=item.get_embed())
+                await item.add_reactions(msg)
+                break
 
 
 @bot.event
 async def on_message(m):
-    if "pussy" in m.content.lower() or ("puss" in m.content.lower() and m.author.id == 483880415124520971):
-        print(m.content)
-        await m.delete()
-    if m.channel.id in [593131220041465868, 593712450487058433, 593816541464625205] and not m.author.bot:
+    if m.author.bot:
+        return
+
+    swear = False
+
+    swears = list()
+    with open("files/swear_words.json") as f:
+        swears = json.loads(f.read())
+
+    for word in m.content.split(" "):
+        if not swear:
+            for result in fuzzle.find(swears, word):
+                if result["accuracy"] > 0.9:
+                    print(result)
+                    swear = True
+                    break
+        else:
+            break
+
+    if swear: await m.channel.send("Watch yo mouth!")
+    if swear and m.author.id in [483880415124520971]: await m.delete()
+
+    if m.channel.category is not None and m.channel.category.id == 594461395039551519:
         item = bh.get_item(m.content)
         if item is not None:
-            for react in item.get_reactions():
-                try:
-                    await msg.add_reaction(react.emoji)
-                except:
-                    continue
+            await item.add_reactions(m)
     await bot.process_commands(m)
 
 
 @bot.event
 async def on_raw_reaction_add(p):
     c = bot.get_channel(p.channel_id)
+    if c.category is None or c.category.id != 594461395039551519:
+        return
     u = c.guild.get_member(p.user_id)
     if u.bot or u.id != 383657174674702346:
         return
